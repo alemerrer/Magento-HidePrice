d2105f75054ce1027fcfd9f23463752b2c00947d
alemerrer <a.lemerrer@alkante.com>
2022-02-12 11:02:30 +0100

    Conversion en plugin

--- Plugin/PluginTest.php
+++ Plugin/PluginTest.php
@@ -1,23 +1,21 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+namespace Alex\HidePrice\Plugin;
 
-namespace Alex\HidePrice\Pricing\Render;
 
+use Magento\Framework\Pricing\Amount\AmountInterface;
+use Magento\Catalog\Pricing\Render\FinalPriceBox;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
-/**
- * Default price box renderer
- */
-class MyNewClass
+class PluginTest
 {
-    public function __construct(ScopeConfigInterface $scopeConfigInterface) {
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfigInterface) {
         $this->scopeConfigInterface =$scopeConfigInterface;
     }
-
-    public function getParameters(): string
+    /**
+     *
+     * 
+     */
+    public function afterRenderAmount()
     {
         $config_val =  $this->scopeConfigInterface->getValue('Alex/general/alex_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         $return = $config_val ? $config_val : '';
@@ -25,4 +23,3 @@ class MyNewClass
         return $return;
     }
 }
-
